4213503e46e44efdba599e3e1e8f41e11ca09a60
Noah Pritchard <noahpritchard9@gmail.com>
2023-06-25 19:25:16 -0400

    set up post req

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-use actix_web::{get, middleware::Logger, web::Path, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    get,
+    middleware::Logger,
+    web::{post, resource, Json, Path},
+    App, HttpResponse, HttpServer, Responder,
+};
 use env_logger::Env;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Person {
     name: String,
     age: u8,
 }
 
+async fn submit(data: Json<Person>) -> impl Responder {
+    log::info!("Received {:?}", data);
+    HttpResponse::Ok()
+}
+
 #[get("/{name}")]
-async fn hello(name: Path<String>) -> impl Responder {
+async fn get_account(name: Path<String>) -> impl Responder {
     log::info!("Request to /{}", name);
     HttpResponse::Ok().json(Person {
         name: name.to_string(),
@@ -20,8 +30,13 @@ async fn hello(name: Path<String>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    HttpServer::new(|| App::new().wrap(Logger::default()).service(hello))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(get_account)
+            .service(resource("/create_account").route(post().to(submit)))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
